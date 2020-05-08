e95da96d1cb76833e539ccd89e916a7c8a4e8332
unknown <santoshbirje8@gmail.com>
2020-05-08 21:10:22 +0530

    Second commit

---
+++ pytest/pytest.py
@@ -0,0 +1,10 @@
+def Get_name(my_name):
+    """
+    This function give your name
+    Args:
+        my_name (str) : this variable takes person name
+    Returns:
+        None
+    """
+    print("this is {}".format(my_name))
+
